efec91a8f34609f1764aedff1bb9494aef370bcb
Ragu Karuturi <ragukaruturi@microsoft.com>
2025-06-17 13:00:41 -0500

    check2

--- startup.sh
+++ startup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo "***********************************************************************"
+echo "********************************** statring custom script *************************************"
 # Make sure your custom glibc is used only by your app
-export LD_LIBRARY_PATH=/tmp/@{APPID}/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${APP_PATH}/lib:$LD_LIBRARY_PATH
+
+echo $LD_LIBRARY_PATH
 
 # daphne -b 0.0.0.0 -p 8000 myproject.asgi:application >> /home/site/wwwroot/daphne-log.txt 2>&1
 gunicorn myapp.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 600
